53b7d4554bac34b069e153c55f4ecc02f3649bb9
Mutlucan Gokcukur <211307006@uzem.education>
2022-07-04 01:31:16 +0300

    Proje dosyası ekle.

---
+++ SimpleBankAccountLogin/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace SimpleBankAccountLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sifre = 1234;
+            int giris_hakkı = 3;
+            int giris_sifre = 0;
+            decimal bakiye = 1000;
+        bb:
+
+            try
+            {
+                Console.Write("Şifreyi Giriniz: ");
+                giris_sifre = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Şifre Sadece Sayılardan Oluşmaktadır");
+                goto bb;
+            }
+
+            if (sifre == giris_sifre)
+            {
+                Console.WriteLine("Sisteme Başarılı Bir Şekilde Giriş Yapıldı..");
+            }
+            else
+            {
+                Console.WriteLine("Yanlış Şifre Girildi");
+                giris_hakkı = giris_hakkı - 1;
+                if (giris_hakkı == 0)
+                {
+                    Console.Write("Giriş Hakkınız Kalmamıştır Sistem Sonlandırılıyor....");
+                    goto aa;
+                }
+                else
+                {
+                    goto bb;
+                }
+            }
+        ff:
+            İslemler.menü();
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 1:
+                    İslemler.bakiyesorgu(bakiye);
+                    break;
+                case 2:
+                    Console.Write("Çekilecek Miktarı Giriniz: ");
+                    decimal cekme = Convert.ToDecimal(Console.ReadLine());
+                    bakiye = İslemler.paracekme(bakiye, cekme);
+                    break;
+                case 3:
+                    Console.Write("Yatırmak İstediğiniz Miktarı Giriniz: ");
+                    decimal yatırma = Convert.ToDecimal(Console.ReadLine());
+                    bakiye = İslemler.parayatırma(bakiye, yatırma);
+                    break;
+                case 4:
+                hh:
+                    Console.Write("Eski Şifrenizi Giriniz: ");
+                    int _sifre = Convert.ToInt32(Console.ReadLine());
+                    if (_sifre != sifre)
+                    {
+                        Console.WriteLine("Eski Şifreyi Yanlış Girdiniz\nTekrar Giriniz");
+                        goto hh;
+                    }
+                gg:
+                    if (giris_sifre == _sifre)
+                    {
+                        try
+                        {
+                            Console.Write("Değiştirmek İstediğiniz 4 Basamaklı Yeni Şifrenizi Giriniz: ");
+                            int yeni_sifre = Convert.ToInt32(Console.ReadLine());
+                            if (yeni_sifre == giris_sifre)
+                            {
+                                Console.WriteLine("Yeni Şifre İle Eski Şifre Aynı Olmamalıdır....");
+                                goto gg;
+                            }
+                            else if (yeni_sifre.ToString().Length != 4)
+                            {
+                                Console.WriteLine("Şifreniz 4 Haneden Oluşmalıdır.\nTekrar Deneyiniz");
+                                goto gg;
+                            }
+                            else
+                            {
+                                System.Threading.Thread.Sleep(2000);
+                                Console.WriteLine("Şifreniz Başarılı Bir Şekilde Değiştirilmiştir.");
+                                giris_sifre = yeni_sifre;
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Şifre Sadece Rakamlardan Oluşmalıdır....");
+                            goto gg;
+                        }
+                    }
+
+                    else if (giris_sifre != _sifre)
+                    {
+                        giris_hakkı = giris_hakkı - 1;
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz İşlem Numarası Girildi\nTekrar Giriş Yapınız...");
+                    goto ff;
+            }
+            Console.Write("\nBaşka Bir İşlem Yapmak İster Misiniz?(E/H)");
+            string tekrarislem = Console.ReadLine();
+            tekrarislem = tekrarislem.ToUpper();
+            if (tekrarislem == "E")
+            {
+                Console.Clear();
+                goto ff;
+            }
+            else
+            {
+                goto aa;
+            }
+        aa:;
+            Console.Write("\nProgram Sonlandırılıyor");
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
---
+++ SimpleBankAccountLogin/İslemler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBankAccountLogin
+{
+    class İslemler
+    {
+        public static void menü()
+        {
+            Console.WriteLine("1-Bakiye Görüntüleme\n2-Para Çekme\n3-Para Yatırma\n4-Şifre Değiştirme");
+            Console.Write("\nYapılacak İşlemi Seçiniz: ");
+        }
+        public static void bakiyesorgu(decimal bakiye)
+        {
+            Console.WriteLine("Hesabınızdaki Bakiye Miktarı: {0}", bakiye);
+        }
+        public static decimal paracekme(decimal bakiye, decimal cekilecekmiktar)
+        {
+            if (cekilecekmiktar > bakiye)
+            {
+                Console.WriteLine("Çekmek İstediğiniz Miktar Hesabınızda Bulunmamaktadır...");
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(2000);
+                bakiye = bakiye - cekilecekmiktar;
+                Console.Write("İstenilen Miktar Başarılı Bir Şekilde Çekilmiştir");
+            }
+            return bakiye;
+        }
+        public static decimal parayatırma(decimal bakiye, decimal yatacakmiktar)
+        {
+            System.Threading.Thread.Sleep(2000);
+            bakiye = bakiye + yatacakmiktar;
+            Console.WriteLine("İşleminiz Başarılı Bir Şekilde Gerçekleşmiştir\nYeni Bakiyenizi Görmek İster Misiniz?(E/H)");
+            string sorgutekrar = Console.ReadLine();
+            sorgutekrar = sorgutekrar.ToUpper();
+            if (sorgutekrar == "E")
+            {
+                bakiyesorgu(bakiye);
+            }
+            return bakiye;
+        }
+    }
+}
